149c55033fa4b6f42a95420268c0536ac4627cd9
Jiya <coderaholic21@gmail.com>
2024-02-16 22:46:07 +0530

    feat: Added Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Team from './pages/Team';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className='bg-black w-screen h-screen text-white '>
-       This is Page 1
+    <div className='app'>
+         <BrowserRouter>
+      {/* <Navbar/> */}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/team' element={<Team/>} />
+        
+      </Routes>
+      {/* <Footer/> */}
+      </BrowserRouter>
     </div>
   )
 }
